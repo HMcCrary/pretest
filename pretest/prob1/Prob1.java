7973f97dd4cd96f8fb5e7383eb5c292d52ed8a25
Hannah McCrary <hmccra100@uafs.edu>
2026-08-21 09:46:34 -0500

    Added code to projects.

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -4,7 +4,22 @@ public class Prob1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		int[] numbers = {4, 7, 2, 9, 1};
 
+	}
+	public static int findMaxDifference(int[] numbers) {
+		int max = 0;
+		int min = 0;
+		
+		if (numbers.length < 2) {
+			return 0;
+		} else {
+			for(int i = 0; i < numbers.length; i++) {
+				if (numbers[i] > i) {
+					 numbers[i] = max;
+				}
+			}
+		}
 	}
 
 }
--- pretest/prob2/Prob2.java
+++ pretest/prob2/Prob2.java
@@ -3,8 +3,16 @@ package prob2;
 public class Prob2 {
 
 	public static void main(String[] args) {
+		String str = "Steam engine in heavy rain";
 		
+		countVowelSequence(str);
 
+	}
+	public static void countVowelSequence(String str) {
+		for(int i = 0; i < str.length(); i++) {
+			System.out.println(str.charAt(i));
+	
+		}
 	}
 
 }
